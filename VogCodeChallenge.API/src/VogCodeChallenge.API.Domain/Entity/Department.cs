4578138a192ca6451fae34950a743304424fd35a
Jean Armstrong <bjarmstr@yahoo.com>
2023-02-25 13:15:44 -0700

    VOG-q2 department address is unique

--- VogCodeChallenge.API/src/VogCodeChallenge.API.Domain/Entity/Department.cs
+++ VogCodeChallenge.API/src/VogCodeChallenge.API.Domain/Entity/Department.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VogCodeChallenge.API.Domain.Entity
 {
-    internal class Department
+    /// <summary>
+    /// Department Entity
+    /// </summary>
+    [Index(nameof(Address), IsUnique = true)]
+
+    public class Department : BaseEntity<Guid>
     {
+        /// <summary>
+        /// Department Name
+        /// </summary>
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Each Department has a Unique Address
+        /// </summary>
+        [Required]
+        public string Address { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Employees in the Department
+        /// </summary>
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
     }
 }
