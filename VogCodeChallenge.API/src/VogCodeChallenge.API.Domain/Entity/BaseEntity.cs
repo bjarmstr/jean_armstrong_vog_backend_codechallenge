e1d97fe574cb5bf85ed73727bbe339240f122b40
Jean Armstrong <bjarmstr@yahoo.com>
2023-02-25 12:44:06 -0700

    VOG-q2 base entity and employee

---
+++ VogCodeChallenge.API/src/VogCodeChallenge.API.Domain/Entity/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Domain.Entity
+{
+    /// <summary>
+    /// Base Entity that other Entities can Inherit from
+    /// </summary>
+    /// <typeparam name="TPrimaryKey"></typeparam>
+    public abstract class BaseEntity<TPrimaryKey>
+    {
+        /// <summary>
+        /// PrimaryKey
+        /// </summary>
+        public TPrimaryKey Id { get; set; }
+    }
+}
---
+++ VogCodeChallenge.API/src/VogCodeChallenge.API.Domain/Entity/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VogCodeChallenge.API.Domain.Entity
+{
+    /// <summary>
+    /// Employee Entity
+    /// </summary>
+    public class Employee : BaseEntity<Guid>
+    {
+
+        /// <summary>
+        /// First Name of Employee
+        /// </summary>
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Last Name of Employee
+        /// </summary>
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Job Title for Employee
+        /// </summary>
+        [Required]
+        public string JobTitle { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Mailing Address
+        /// </summary>
+        [Required]
+        public string Address { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Department that employee is assigned to
+        /// </summary>
+        [Required]
+        public Guid DepartmentId { get; set; }
+
+    }
+}
