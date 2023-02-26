cf3811edeb822a486dac10c0cefc29811ef8f92a
Jean Armstrong <bjarmstr@yahoo.com>
2023-02-25 21:33:23 -0700

    VOG-q8 sum odd numbers and console write

---
+++ VogCodeChallenge.ConsoleApp/OddNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.ConsoleApp
+{
+    public static class OddNumbers
+    {
+
+        public static long SumOddNumbers()
+        {
+            var sum = 0;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            return sum;
+        }
+
+    }
+}
+
--- VogCodeChallenge.ConsoleApp/Program.cs
+++ VogCodeChallenge.ConsoleApp/Program.cs
@@ -1,9 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using VogCodeChallenge.ConsoleApp;
 
-    Console.WriteLine("Question7");
-Console.WriteLine();
+
+Console.WriteLine("Question7");
 var namesList = QuestionClass.NamesToAllCaps();
-    Console.WriteLine(string.Join(" ", namesList));
+Console.WriteLine(string.Join(" ", namesList));
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Question8");
+var sum = OddNumbers.SumOddNumbers();
+Console.WriteLine(sum);
 
 
