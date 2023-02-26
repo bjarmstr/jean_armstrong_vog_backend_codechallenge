a8c640ea072cbd5b57e726b63d38b8aeea6de324
Jean Armstrong <bjarmstr@yahoo.com>
2023-02-25 19:44:22 -0700

    VOG-q5 make all employee fields requiredin VM

--- VogCodeChallenge.API/src/VogCodeChallenge.API.Restful/ViewModels/Employee/v1_0/Request/CreateEmployeeRequest_v1_0.cs
+++ VogCodeChallenge.API/src/VogCodeChallenge.API.Restful/ViewModels/Employee/v1_0/Request/CreateEmployeeRequest_v1_0.cs
@@ -1,4 +1,6 @@
-﻿namespace VogCodeChallenge.API.Restful.ViewModels.Employee.v1_0.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VogCodeChallenge.API.Restful.ViewModels.Employee.v1_0.Request
 {
     public class CreateEmployeeRequest_v1_0
     {
@@ -6,26 +8,31 @@
         /// <summary>
         /// First Name of Employee
         /// </summary>
+        [Required]
         public string FirstName { get; set; } = string.Empty;
 
         /// <summary>
         /// Last Name of Employee
         /// </summary>
+        [Required]
         public string LastName { get; set; } = string.Empty;
 
         /// <summary>
         /// Job Title for Employee
         /// </summary>
+        [Required]
         public string JobTitle { get; set; } = string.Empty;
 
         /// <summary>
         /// Mailing Address
         /// </summary>
+        [Required]
         public string Address { get; set; } = string.Empty;
 
         /// <summary>
         /// Department that employee is assigned to
         /// </summary>
+        [Required]
         public Guid DepartmentId { get; set; }
     }
 }
