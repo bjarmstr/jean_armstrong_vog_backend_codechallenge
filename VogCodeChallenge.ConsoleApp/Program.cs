6463037227a5128f523e77fcf40f694ed1d3f36d
Jean Armstrong <bjarmstr@yahoo.com>
2023-02-25 22:09:15 -0700

    VOG-q9 switch statment with console output

--- VogCodeChallenge.ConsoleApp/Program.cs
+++ VogCodeChallenge.ConsoleApp/Program.cs
@@ -11,5 +11,20 @@ Console.WriteLine();
 Console.WriteLine("Question8");
 var sum = OddNumbers.SumOddNumbers();
 Console.WriteLine(sum);
+Console.WriteLine();
+Console.WriteLine();
 
-
+Console.WriteLine("Question9");
+Console.WriteLine("A input of 3 returns " + SWITCHModules.SWITCHModule(3));
+Console.WriteLine("A input of 5 returns " + SWITCHModules.SWITCHModule(5));
+try
+{
+    var negativeInt = SWITCHModules.SWITCHModule(-1);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("A input of -1 returns " + ex.Message);
+}
+Console.WriteLine("A input of 1.0f returns " + SWITCHModules.SWITCHModule(1.0f));
+Console.WriteLine("A input of mystring returns " + SWITCHModules.SWITCHModule("mystring"));
+Console.WriteLine("An input of 1.0m returns  " + SWITCHModules.SWITCHModule(1.0m));
---
+++ VogCodeChallenge.ConsoleApp/SWITCHModules.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.ConsoleApp
+{
+    public static class SWITCHModules
+    {
+
+        public static dynamic SWITCHModule(dynamic value)
+        {
+            switch (value)
+            {
+                case int oneToFour when oneToFour > 0 && oneToFour <= 4:
+                    return oneToFour * 2;
+
+                case int greaterThanFour when greaterThanFour > 4:
+                    return greaterThanFour *3;
+
+                case int below1 when below1 < 1 :
+                    throw new Exception("Exception: value below 1");
+
+                case float oneOrTwoFloat when oneOrTwoFloat == 1.0f || oneOrTwoFloat == 2.0f:
+                    return 3.0f;
+
+                case string stringValue:
+                    return stringValue.ToUpper();
+
+                default:
+                    return value;
+            }
+
+        }
+
+    }
+}
+
