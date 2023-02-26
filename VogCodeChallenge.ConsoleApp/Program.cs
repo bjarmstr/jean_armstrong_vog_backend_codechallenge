1b56adca51813f524d539499a842185478132c36
Jean Armstrong <bjarmstr@yahoo.com>
2023-02-25 20:45:18 -0700

    VOG-q7 console app returns list

---
+++ VogCodeChallenge.ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using VogCodeChallenge.ConsoleApp;
+
+    Console.WriteLine("Question7");
+Console.WriteLine();
+var namesList = QuestionClass.NamesToAllCaps();
+    Console.WriteLine(string.Join(" ", namesList));
+
+
---
+++ VogCodeChallenge.ConsoleApp/QuestionClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.ConsoleApp
+{
+    public static class QuestionClass
+    {
+        static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffery",
+            "John",
+        };
+
+        public static List<string>  NamesToAllCaps()
+        {
+            var test = NamesList;
+              
+
+            return test;
+        }
+
+    }
+}
