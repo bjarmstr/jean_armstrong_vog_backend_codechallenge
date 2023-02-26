6519f10e3dd3687fad6eb1ebda4e2cf0830de058
Jean Armstrong <bjarmstr@yahoo.com>
2023-02-26 06:59:04 -0700

    VOG-q12 find radius using reflection

---
+++ VogCodeChallenge.ConsoleApp/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.ConsoleApp
+{
+    public sealed class Circle
+    {
+        private double radius;
+
+        public double CalculateDiameter(Func<double, double> operation)
+        {
+            return operation(radius);
+        }
+
+    }
+}
