07ea57154d2c121f6647b2c23417c83267b44562
Jean Armstrong <bjarmstr@yahoo.com>
2023-02-25 23:10:05 -0700

    VOG-q10 test for switch statements

---
+++ VogCodeChallenge.Test/SWITCHModulesTest.cs
@@ -0,0 +1,36 @@
+using Newtonsoft.Json.Linq;
+using VogCodeChallenge.ConsoleApp;
+
+namespace VogCodeChallenge.Test
+{
+    public class SWITCHModulesTest
+    {
+        [Theory]
+        //A. int values of 1,2,3,4, multiply the provided value by 2 
+        [InlineData(2, 4)]
+        //B. int values bigger than 4, multiply the provided value by 3
+        [InlineData(10, 30)]
+        //D. float values 1.0f and 2.0f returns 3.0f
+        [InlineData(2.0f, 3.0f)]
+        //E. any string values convert to UpperCase
+        [InlineData("myString", "MYSTRING")]
+        //F. anything else return the input value
+        [InlineData(2.03, 2.03)]
+        public void SumOddNumbers_ReturnsSimillarValueType_WhenCalled(dynamic value1, dynamic expected)
+        {
+            var result = SWITCHModules.SWITCHModule(value1);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void SumOddNumbers_ThrowsException_WhenCalledWithNumberLowerThan1()
+        {
+
+            var ex = Assert.Throws<Exception>(() => SWITCHModules.SWITCHModule(-3));
+            Assert.Contains("value below 1", ex.Message);
+
+        }
+
+    }
+}
